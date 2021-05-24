f6b8b90d98b0f61c6d2270820466bce3fbc907ef
Alexandr <speedo210@gmail.com>
2021-05-24 14:14:46 +0300

    upd

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(function(){
-  $('.slider').slick({
-    arrows: false,
-    dots: true
+  $('.intro_slider').slick({
+    adaptiveHeight:true,
+    accessibility: false,
+    slidesToShow: 1,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    responsive: [{
+      brealpoint: 768,
+      settings : {}
+    }]
   });
 
 });
